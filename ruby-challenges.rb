f6f3a8a171e0dae0e22ca45baaded71105ad68b7
LEARN Academy <learnacademy@LEARNs-MacBook-Pro.local>
2023-03-10 13:11:45 -0800

    Completed week 4 assessment

--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,8 +10,6 @@ num2 = 42
 num3 = 221
 # Expected output: '221 is odd'
 
-
-
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. 
 # HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
 
